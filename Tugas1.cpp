441d66cc06d3e0acefa651f035f6e2dfd5e7dc8a
Daffa Wiratama <159789661+DaffaWiratama@users.noreply.github.com>
2026-09-16 17:45:19 +0700

    Menambahkan fitur pengkategorian suhu

--- Tugas1.cpp
+++ Tugas1.cpp
@@ -10,20 +10,75 @@ int main(){
     
     cout<<"Masukkan nilai suhu: "<<endl;
     cin>>nilaisuhu;
-    cout<<"Anda ingin melihat data dalam satuan apa? (C/F/K)"<<endl;
+    cout<<"Apa kode satuan suhu yang anda pakai? (C/F/K) "<<endl;
     cin>>satuan;
 
     kodesatuan = (kodesatuan >= 'a' && kodesatuan <= 'z') ? kodesatuan - ('a' - 'A') : kodesatuan;
     switch kodesatuan {
         case 'C':
-            cout<<"Suhu dalam derajat Celsius: "<<suhu<<endl;
-        case 'F':
-            F=9/5.0*suhu+32;
+            C=nilaisuhu;
+            if (C<(-273.15)){
+                cout<<"ERR: SUHU TIDAK MUNGKIN"<<endl;
+                return 0;
+            }
+            cout<<"Suhu dalam derajat Celsius: "<<C<<endl;
+            F=9/5.0*C+32;
             cout<<"Suhu dalam derajat Fahrenheit: "<<F<<endl;
-        case 'K':
-            K=suhu+273.15;
+            K=C+SELISIH_KELVIN;
             cout<<"Suhu dalam Kelvin: "<<K<<endl;
+            if (C<10){
+                cout<<"Kategori suhu: DINGIN"<<endl;
+            } else if (C>=10 && C<25){
+                cout<<"Kategori suhu: Sejuk"<<endl;
+            } else if (C>=25 && C<32){
+                cout<<"Kategori suhu: HANGAT"<<endl;
+            } else if (C>=32){
+                cout<<"Kategori suhu: PANAS"<<endl;
+            }
+            break;
+        case 'F':
+            C=(nilaisuhu-32)*5/9.0;
+            if (C<(-273.15)){
+                cout<<"ERR: SUHU TIDAK MUNGKIN"<<endl;
+                return 0;
+            }
+            cout<<"Suhu dalam derajat Celsius: "<<C<<endl;
+            F=9/5.0*C+32;
+            cout<<"Suhu dalam derajat Fahrenheit: "<<F<<endl;
+            K=C+SELISIH_KELVIN;
+            cout<<"Suhu dalam Kelvin: "<<K<<endl;
+            if (C<10){
+                cout<<"Kategori suhu: DINGIN"<<endl;
+            } else if (C>=10 && C<25){
+                cout<<"Kategori suhu: Sejuk"<<endl;
+            } else if (C>=25 && C<32){
+                cout<<"Kategori suhu: HANGAT"<<endl;
+            } else if (C>=32){
+                cout<<"Kategori suhu: PANAS"<<endl;
+            }
+            break;
+        case 'K':
+            C=nilaisuhu-SELISIH_KELVIN;
+            if (C<(-273.15)){
+                cout<<"ERR: SUHU TIDAK MUNGKIN"<<endl;
+                return 0;
+            }
+            cout<<"Suhu dalam derajat Celsius: "<<C<<endl;
+            F=9/5.0*C+32;
+            cout<<"Suhu dalam derajat Fahrenheit: "<<F<<endl;
+            K=C+SELISIH_KELVIN;
+            cout<<"Suhu dalam Kelvin: "<<K<<endl;
+            if (C<10){
+                cout<<"Kategori suhu: DINGIN"<<endl;
+            } else if (C>=10 && C<25){
+                cout<<"Kategori suhu: Sejuk"<<endl;
+            } else if (C>=25 && C<32){
+                cout<<"Kategori suhu: HANGAT"<<endl;
+            } else if (C>=32){
+                cout<<"Kategori suhu: PANAS"<<endl;
+            }
+            break;
         default:
-            cout<<"KODE SATUAN TIDAK DIKENAL";
+            cout<<"ERR: KODE SATUAN TIDAK DIKENAL";
     }
 }
